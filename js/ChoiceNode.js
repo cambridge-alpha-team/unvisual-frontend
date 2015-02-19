a4453d05da837511b96136b1cb3f23bcc384ad8e
James Baker <j_d_baker@hotmail.com>
2015-02-19 21:32:07 +0000

    Remove tree levels
    Removes an unnecessary tree level from Sample and Synth

--- js/ChoiceNode.js
+++ js/ChoiceNode.js
@@ -1,8 +1,10 @@
-function ChoiceNode(name, parent, choices) {
+function ChoiceNode(name, parent, choices, childNumber) {
 	this.name = name;
 	this.parent = parent || null;
 	this.children = [];
-	if(parent != null) {
+	if(childNumber != null) {
+		parent.children.splice(childNumber, 0, this);
+	} else if(parent != null) {
 		parent.children.splice(0, 0, this);
 	}
 	
@@ -12,11 +14,17 @@ function ChoiceNode(name, parent, choices) {
 inherits(ChoiceNode, Node);
 
 ChoiceNode.prototype.readName = function() {
-	return this.name + " " + this.choice;
+	return this.name + " " + this.choice;	
 };
 
 ChoiceNode.prototype.generateCode = function() {
-	return this.choice;
+	if(this.name == "sample") {
+		return "sample :" + this.choice;
+	} else if(this.name == "synth") {
+		return "use_synth :" + this.choice;
+	} else {
+		return this.choice;
+	}
 };
 
 ChoiceNode.prototype.generateHTML = function() {
@@ -24,7 +32,13 @@ ChoiceNode.prototype.generateHTML = function() {
 	if(this == activeNode) {
 		sonicPi += '<pre style="font-size: 1em; border: black 2px solid; display: inline">';
 	}
-	sonicPi += this.choice;
+	if(this.name == "sample") {
+		sonicPi += "sample :" + this.choice;
+	} else if(this.name == "synth") {
+		sonicPi += "use_synth :" + this.choice;
+	} else {
+		sonicPi += this.choice;
+	}
 	if(this == activeNode) {
 		sonicPi += '</pre>';
 	}
--- js/SampleNode.js
+++ js/SampleNode.js
@@ -1,53 +1,5 @@
 function SampleNode(parent, childNumber) {
-	this.name = 'sample';
-	this.parent = parent || null;
-	this.children = [];
-	if(childNumber != null) {
-		parent.children.splice(childNumber, 0, this);
-	} else if(parent != null) {
-		parent.children.push(this);
-	}
-	
-	new ChoiceNode('sample name', this, ['bass_hit_c', 'loop_industrial', 'guit_harmonics']);
+	return new ChoiceNode('sample', parent, ['bass_hit_c', 'loop_industrial', 'guit_harmonics'], childNumber);
 }
 inherits(SampleNode, Node);
 
-SampleNode.prototype.readName = function() {
-	if(this.children[0] instanceof ValueNode || this.children[0] instanceof ChoiceNode) {
-		return this.name + " " + this.children[0].choice;
-	} else {
-		return this.name;
-	}
-};
-
-SampleNode.prototype.generateCode = function() {
-	var sonicPi = "sample :" + this.children[0].generateCode() + " ";
-	for (var i = 1; i < this.children.length; i++) {
-		if(i != 1) sonicPi += ", ";
-		
-		sonicPi += this.children[i].generateCode();
-	}
-	return sonicPi;
-};
-
-SampleNode.prototype.generateHTML = function() {
-	var sonicPi = '';
-	if(this == activeNode) {
-		sonicPi += '<pre style="display: inline; font-size: 1em; border: black 2px solid">';
-	}
-	
-	sonicPi += "sample :" + this.children[0].generateHTML();
-	for (var i = 1; i < this.children.length; i++) {
-		if(i == 1) {
-			sonicPi += " ";
-		} else {
-			sonicPi += ", ";
-		}
-		sonicPi += this.children[i].generateHTML();
-	}
-	
-	if(this == activeNode) {
-		sonicPi += '</pre>';
-	}
-	return sonicPi;
-};
